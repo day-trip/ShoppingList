57c7ac63bd100c2f0ac7557b23c6f9d7f671abdf
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 19:23:03 -0800

    first committ

--- src/sendToLogin.js
+++ src/sendToLogin.js
@@ -2,7 +2,7 @@ function sendToLogin(next) {
     if (next !== null && next !== undefined) {
         window.localStorage.setItem("next", next);
     }
-    window.location.href = "https://jcclol.auth.us-east-1.amazoncognito.com/login?client_id=33vptobuge4u71evls6c12ru5b&response_type=token&scope=email+openid+phone&redirect_uri=http://localhost:3000/callback";
+    window.location.href = "https://jcclol.auth.us-east-1.amazoncognito.com/login?client_id=33vptobuge4u71evls6c12ru5b&response_type=token&scope=email+openid+phone&redirect_uri=https://jcc.lol/callback";
 }
 
 export default sendToLogin;
